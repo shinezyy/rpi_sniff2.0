312dd566beee09aa01b01b9b18ba534e7dbf9bbd
shinezyy <diamondzyy@sina.com>
2016-11-30 21:05:54 +0800

    use environ for piid

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+import os
+
 PC_test = True
 server_ip_addr = '172.26.119.237:18000'
 send_to_server = True
@@ -12,6 +14,7 @@ ip_start = ''
 def init():
     global WLAN_SSID
     global ip_start
+    pi_id = int(os.environ['pi_id'])
 
     if test_in_lab:
         WLAN_SSID = "124"
