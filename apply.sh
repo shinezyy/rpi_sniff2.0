a20d6a67a183456e722fcb226e5701b88ab53cd3
shinezyy <diamondzyy@sina.com>
2016-12-01 16:05:51 +0800

    add some notice

--- apply.sh
+++ apply.sh
@@ -1,5 +1,8 @@
 pi_path=$1
 
+echo "Remember to add one parameter"
+echo "Remember to modify PC_test and test_in_lab in config file"
+
 if [ $pi_path = "" ] ; then
     echo Not allowed;
     exit 1;
