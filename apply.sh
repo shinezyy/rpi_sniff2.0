657f63f3c0151c37415ced9a958428f78187ed0c
shinezyy <diamondzyy@sina.com>
2016-12-01 21:39:08 +0800

    mv files that will never be used

--- apply.sh
+++ apply.sh
@@ -7,15 +7,7 @@ if [ $pi_path = "" ] ; then
     echo Not allowed;
     exit 1;
 else
-    echo sudo cp ./interfaces $pi_path/etc/network/
-    echo sudo cp ./rc.local $pi_path/etc/
-    echo sudo cp ./environment $pi_path/etc/
-    echo cp ./start.sh $pi_path/home/pi/
     echo cp ./src $pi_path/home/pi/ -r
 
-    sudo cp ./interfaces $pi_path/etc/network/
-    sudo cp ./rc.local $pi_path/etc/
-    sudo cp ./environment $pi_path/etc/
-    cp ./start.sh $pi_path/home/pi/
     cp ./src $pi_path/home/pi/ -r
 fi
